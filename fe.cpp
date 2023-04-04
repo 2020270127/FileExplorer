bcdef9c0e1ab735251de76983bdc9471d00bfe42
Bae Junbeom <beajun4958@gmail.com>
2023-04-04 18:20:47 +0900

    argparser feature

--- fe.cpp
+++ fe.cpp
@@ -1,3 +1,4 @@
+#include "include/argparse.hpp"
 #include <filesystem>
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,10 +7,27 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        printf("usage: fe [command] -d [dirname]\n");
-        exit(0);
+    argparse::ArgumentParser program("FileExplorer");
+
+    // Args list
+    program.add_argument("-dir").default_value("./").help("target dir");
+    program.add_argument("-mkdir").help("make dir");
+    program.add_argument("-rm").help("remove file or dir");
+    program.add_argument("-touch").help("touch");
+    program.add_argument("-cp").help("copy src to dst");
+    program.add_argument("-mv").help("move and");
+    program.add_argument("-sort").help("sort list [algorithm]");
+    program.add_argument("-ss").help("search by substring");
+
+    program.parse_args(argc, argv);
+
+    if(program.is_used("-rm")){
+
     }
+    else if(program.is_used("-dir")){
+        printf("%s", program.get<string>("-dir"));
+    }
+
 
     exit(0);
 }
