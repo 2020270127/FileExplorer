5a0799b3a06e4f6512e2330fa1d9857b75808b48
2020270127 <tottenham2968@gmail.com>
2023-05-02 10:19:03 +0900

    -cp,-mv 구현

--- fe.cpp
+++ fe.cpp
@@ -16,12 +16,13 @@ int main(int argc, char *argv[]) {
     program.add_argument("-mkdir").help("make dir"); //폴더 생성
     program.add_argument("-rm").help("remove file or dir"); //파일 혹은 폴더 삭제 
     program.add_argument("-touch").help("touch"); //파일 생성
-    program.add_argument("-cp").help("copy src to dst"); //파일 혹은 폴더 복사
-    program.add_argument("-mv").help("move and"); //이동
+    program.add_argument("-cp").help("copy src to dst").nargs(2); //파일 혹은 폴더 복사
+    program.add_argument("-mv").help("move and").nargs(2); //이동
+
     program.add_argument("-sort").help("sort list [algorithm]"); //정렬
     program.add_argument("-ss").help("search by substring"); //검색
 
-
+   
   try {
     program.parse_args(argc, argv);
   } catch (const std::runtime_error &err) {
@@ -75,9 +76,80 @@ int main(int argc, char *argv[]) {
     }
     }
 
-    else if (program.is_used("-cp")) {
-        //cp
+    else if (program.is_used("-cp")) {   
+
+    namespace fs = std::filesystem;
+
+
+    std::string file_name = program.get<std::vector<string>>("-cp")[0];
+    
+    std::string destination_path = program.get<std::vector<string>>("-cp")[1];
+
+    fs::path source = fs::current_path();
+    fs::path destination(destination_path);
+
+    if (!fs::exists(source / file_name)) // 소스 경로에 파일이나 폴더가 존재하지 않는 경우
+    {
+        std::cout << "Cannot copy file or folder: " << source / file_name << " (not found)" << std::endl;
+        return 1;
     }
+
+    try
+    {
+        if (fs::is_directory(source / file_name)) // 폴더를 복사하는 경우
+        {
+            fs::create_directories(destination / file_name);
+            for (const auto& file : fs::recursive_directory_iterator(source / file_name))
+            {
+                fs::path current(file.path());
+                fs::path target(destination / file_name / current.lexically_relative(source / file_name));
+                if (fs::is_directory(current)) // 하위 폴더를 만들어줌
+                {
+                    fs::create_directory(target);
+                }
+                else // 파일을 복사함
+                {
+                    fs::copy_file(current, target, fs::copy_options::overwrite_existing);
+                }
+            }
+        }
+        else // 파일을 복사하는 경우
+        {
+            fs::copy_file(source / file_name, destination / file_name, fs::copy_options::overwrite_existing);
+        }
+
+        std::cout << "File or folder copied successfully" << std::endl;
+        return 0;
+    }
+    catch (const std::exception& ex)
+    {
+        std::cerr << "Error: " << ex.what() << std::endl;
+        return 1;
+    }
+}
+
+    else if (program.is_used("-mv")) {
+
+    namespace fs = std::filesystem;
+
+    std::string file_name = program.get<std::vector<string>>("-cp")[0];
+    std::string destination_path = program.get<std::vector<string>>("-cp")[1];
+
+    fs::path current_path = fs::current_path();
+    fs::path source_file = current_path / file_name;
+    fs::path destination_file = current_path / destination_path / file_name;
+
+    try {
+        fs::rename(source_file, destination_file);
+        std::cout << "File moved successfully!" << std::endl;
+    }
+    catch (const std::filesystem::filesystem_error& e) {
+        std::cerr << e.what() << std::endl;
+    }
+}
+
+
+    
 
     else if (program.is_used("-sort")) {
         //sort
