2cb4eb169ca3fc50a1ff37267afd64d72c3de281
2020270127 <69888585+2020270127@users.noreply.github.com>
2023-05-04 16:41:38 +0900

    Update fileinfo.cpp
    이중포인터 제거

--- fileinfo.cpp
+++ fileinfo.cpp
@@ -3,7 +3,6 @@
 #include <string>
 #include <sys/stat.h>
 #include <ctime>
-#include "include/sorting_ldkstellar.hpp"
 using namespace std;
 namespace fs = std::filesystem;
 
@@ -16,13 +15,13 @@ struct FileInfo {
 };
 
 template <typename T>
-void printInfo(T **info_array, int size) {
+void printInfo(T *info_array, int size) {
     for (int i = 0; i < size; i++) {
-        cout << "Name: " << info_array[i]->name << endl;
-        cout << "Path: " << info_array[i]->path << endl;
-        cout << "Size: " << info_array[i]->size << endl;
-        cout << "Modified time: " << std::asctime(std::localtime(&(info_array[i]->modified_time)))<<std::endl;
-        cout << "Type: " << (info_array[i]->is_directory ? "directory" : "file") << endl;
+        cout << "Name: " << info_array[i].name << endl;
+        cout << "Path: " << info_array[i].path << endl;
+        cout << "Size: " << info_array[i].size << endl;
+        cout << "Modified time: " << std::asctime(std::localtime(&(info_array[i].modified_time)))<<std::endl;
+        cout << "Type: " << (info_array[i].is_directory ? "directory" : "file") << endl;
         cout << endl;
     }
 }
@@ -36,24 +35,23 @@ int getSize(){
     }
     return file_count;
 }
-FileInfo** getInfo() {
+FileInfo* getInfo() {
     int file_count = getSize();
 
-    FileInfo **info_array = new FileInfo *[file_count];
+    FileInfo *info_array = new FileInfo[file_count];
     int i = 0;
 
     for (auto& entry : fs::directory_iterator(fs::current_path())) {
         if (entry.is_regular_file() || entry.is_directory()) {
             struct stat st;
-            info_array[i] = new FileInfo;
-            info_array[i]->name = entry.path().filename().string();
-            info_array[i]->path = entry.path().string();
-            info_array[i]->size = entry.is_directory() ? 0 : entry.file_size();
-            if(stat(&(info_array[i]->name)[0],&st) != 0){
+            info_array[i].name = entry.path().filename().string();
+            info_array[i].path = entry.path().string();
+            info_array[i].size = entry.is_directory() ? 0 : entry.file_size();
+            if(stat(&(info_array[i].name)[0],&st) != 0){
                  std::cout << "Failed to get file status" << std::endl;
             }
-            info_array[i]->modified_time = st.st_mtime;
-            info_array[i]->is_directory = entry.is_directory();
+            info_array[i].modified_time = st.st_mtime;
+            info_array[i].is_directory = entry.is_directory();
             i++;
         }
     }
@@ -69,10 +67,7 @@ FileInfo** getInfo() {
 
 }
 int main(){
-    FileInfo** arr;
-    arr = getInfo();
+    FileInfo* arr = getInfo();
     int array_size = getSize();
-    printInfo(arr, array_size);
-    sorting(arr, 0, 0, array_size - 1); // 첫 번째 열(즉, name 속성)로 정렬
     printInfo(arr, array_size);
 }
