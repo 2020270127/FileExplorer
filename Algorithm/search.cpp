d3a1905f0ca819127ebdd302c083b721887b44a7
KIM-GUEN-HEE <luca0879@korea.ac.kr>
2023-05-14 19:41:10 +0900

    kmp

--- Algorithm/search.cpp
+++ Algorithm/search.cpp
@@ -1,6 +1,14 @@
 #include <cstring>
 #include <filesystem>
 #include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+
+#define MAX_STRING_LENGTH 100  // 파일이름의 최대 길이
+#define MAX_PATTERN_LENGTH 100  // 찾을 파일이름의 최대 길이
+#define MAX_STRUCT_COUNT 100   // 파일정보 구조체 배열의 최대 크기
+
 // template 사용하여 여러 type에 대응 가능하도록 함수 설계
 
 template <typename T> void swap(T *a, T *b) {
@@ -8,6 +16,10 @@ template <typename T> void swap(T *a, T *b) {
     a = b;
     b = tmp;
 }
+
+int strstr(char str[], char key[]);
+bool kmp(char text[], char pattern[]);
+
 
 
 int strstr(char str[], char key[]) {
@@ -28,3 +40,72 @@ int strstr(char str[], char key[]) {
     }
     return 0;
 }
+
+
+// 테스트용 구조체 정의
+typedef struct Fileinfo{
+    char name[MAX_STRING_LENGTH + 1];  // 파일 이름
+    int size;                          // 파일 크기
+}Fileinfo;
+
+// KMP 알고리즘 함수
+bool kmp(char text[], char pattern[]) {
+
+    int fail[strlen(pattern)];               // 실패 함수
+    int j = 0;                               // 패턴 문자열의 인덱스
+
+    // 실패 함수 구하기
+    fail[0] = -1;       // 실패함수의 0인덱스의 값은 항상 -1
+    for (int i = 1; i < strlen(pattern); i++) {
+        while (j > 0 && pattern[i] != pattern[j]) {
+            j = fail[j - 1];
+        }
+        if (pattern[i] == pattern[j]) {
+            fail[i] = ++j;
+        } else {
+            fail[i] = 0;
+        }
+    }
+
+    // 문자열 검색하기
+    j = 0;
+    for (int i = 0; i < strlen(text); i++) {
+        while (j > 0 && text[i] != pattern[j]) { 
+            j = fail[j - 1];           
+        }
+        if (text[i] == pattern[j]) {
+            if (++j == strlen(pattern)) {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+//테스토용
+/* int main() {
+    // 구조체 배열 초기화
+    Fileinfo arr[MAX_STRUCT_COUNT] = {
+        {"Hello World", 11},
+        {"This is a test", 14},
+        {"Goodbye", 7},
+        {"Programming", 11},
+        {"This is KMP algorithm", 13}
+    };
+
+    int arr_len = sizeof(arr) / sizeof(Fileinfo);
+
+    char pattern[MAX_PATTERN_LENGTH + 1];  // 패턴 문자열
+    printf("파일 이름을 입력하시오 : ");
+    scanf("%[^\n]", pattern);              // %[^\n] --> 패턴 문자열의 공백 제거
+
+    // 구조체 배열에서 패턴 문자열 찾기
+    for (int i = 0; i < arr_len; i++) {
+        if (kmp(arr[i].name, pattern)) {
+            std::cout<<arr[i].name<<" 파일은 "<<i+1<<"번째에 있습니다."<<std::endl;
+        }
+    }
+
+    return 0;
+} */
