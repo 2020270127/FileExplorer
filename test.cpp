4d901302ba75276c6024033632af9d948612b602
Bae Junbeom <beajun4958@gmail.com>
2023-05-21 18:10:04 +0900

    test code structure

---
+++ test.cpp
@@ -0,0 +1,27 @@
+#include <filesystem>
+#include <iostream>
+#include "Algorithm/sort.hpp"
+using namespace std;
+
+
+int main(){
+    cout << "테스트 시작" << endl;
+    //Sort 테스트
+    // 임의의 구조체 배열 생성
+
+
+    // 3가지 정렬함수로 정렬 후 검사
+
+
+
+    // STRSTR, KMP 테스트
+    // 찾을 임의의 패턴 정하기
+    // 특정 패턴이 포함된 문자열 케이스 생성
+    // 패턴이 포함되지 않은 문자열 생성
+    // 각 함수가 위 문자열에 적절한 값이 리턴되는 지 검사
+
+    //DFS, BFS 검사
+    // 최소 3단계 깊이의 폴더 구조 생성
+    // 깊이에 따라서 파일명 다르게 생성
+    // DFS, BFS가 어떤 순서로 파일명을 출력하는지 검사
+}
