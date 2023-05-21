7b9bdef1055a1c422dd0aee952c1a09a56bc8ef2
2020270127 <tottenham2968@gmail.com>
2023-05-21 19:16:17 +0900

    add sort.hpp test

--- test.cpp
+++ test.cpp
@@ -1,7 +1,11 @@
 #include <filesystem>
 #include <iostream>
+#include <ctime>
 #include "Algorithm/sort.hpp"
+#include "Algorithm/fileinfo.hpp"
 using namespace std;
+
+std::tm timeStruct{};
 
 
 int main(){
@@ -9,8 +13,77 @@ int main(){
     //Sort 테스트
     // 임의의 구조체 배열 생성
 
+    FileInfo fileArray[5]; // 구조체 배열을 선언합니다.
+    std::time_t currentTime = std::time(nullptr);
+    std::srand(static_cast<unsigned int>(currentTime));
+
+    for (int i = 0; i < 5; ++i) {
+        fileArray[i].name = "File" + std::to_string(rand());
+        fileArray[i].size = std::rand() % 100000; 
+        fileArray[i].is_directory = (std::rand() % 2 == 0);
+        fileArray[i].modified_time = currentTime + (std::rand() % 3600);
+    } //랜덤 구조체 생성
 
     // 3가지 정렬함수로 정렬 후 검사
+    // quick_sort check
+    quick_sort(fileArray, 0, 4, &FileInfo::modified_time, comp<int>);
+    for (int i = 0; i < 4; i++) {
+        if(fileArray[i].modified_time > fileArray[i+1].modified_time)
+            printf("error on quick_sort(time)\n"); 
+    }
+    quick_sort(fileArray, 0, 4, &FileInfo::size, comp<int>);
+    for (int i = 0; i < 4; i++) {
+        if(fileArray[i].size > fileArray[i+1].size)
+            printf("error on quick_sort(size)\n"); 
+    }
+    quick_sort(fileArray, 0, 4, &FileInfo::name, comp<string>);
+    for (int i = 0; i < 4; i++) {
+        if(fileArray[i].name > fileArray[i+1].name)
+            printf("error on quick_sort(name)\n"); 
+    }
+
+    // merge_sort check
+    merge_sort(fileArray, 0, 4, &FileInfo::modified_time, comp<int>);
+    for (int i = 0; i < 4; i++) {
+        if(fileArray[i].modified_time > fileArray[i+1].modified_time)
+            printf("error on merge_sort(time)\n"); 
+    }
+    merge_sort(fileArray, 0, 4, &FileInfo::size, comp<int>);
+    for (int i = 0; i < 4; i++) {
+        if(fileArray[i].size > fileArray[i+1].size)
+            printf("error on merge_sort(size)\n"); 
+    }
+    merge_sort(fileArray, 0, 4, &FileInfo::name, comp<string>);
+    for (int i = 0; i < 4; i++) {
+        if(fileArray[i].name > fileArray[i+1].name)
+            printf("error on merge_sort(name)\n"); 
+    }
+
+    // head_sort check
+    heap_sort(fileArray, 5, &FileInfo::modified_time, comp<int>);
+    for (int i = 0; i < 4; i++) {
+        if(fileArray[i].modified_time > fileArray[i+1].modified_time)
+            printf("error on heap_sort(time)\n"); 
+    }
+    heap_sort(fileArray, 5, &FileInfo::size, comp<int>);
+    for (int i = 0; i < 4; i++) {
+        if(fileArray[i].size > fileArray[i+1].size){
+            printf("error on heap_sort(size)\n"); 
+            printInfo(&fileArray[i],1);
+            printInfo(&fileArray[i+1],1);
+        }
+            
+    }
+    heap_sort(fileArray, 5, &FileInfo::name, comp<string>);
+    for (int i = 0; i < 4; i++) {
+        if(fileArray[i].name > fileArray[i+1].name)
+        {
+            printf("error on heap_sort(name)\n"); 
+            printInfo(&fileArray[i],1);
+            printInfo(&fileArray[i+1],1);
+        }
+            
+    }
 
 
 
